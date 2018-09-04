84104c98bfdba01cee0f3d9d8a3571fe6ca6f4b5
zxza <841827568@qq.com>
2018-09-04 11:46:05 +0800

    增加表单组件picker detaildemo

--- client/pages/picker/picker.js
+++ client/pages/picker/picker.js
@@ -6,6 +6,13 @@ Page({
      */
     data: {
         array: ['美国', '中国', '巴西', '日本'],
+        index: 0,
+        multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
+        multiIndex: [0, 0, 0],
+        time: '12:01',
+        date: '2015-09-01',
+        region: ['广东省', '广州市', '海珠区'],
+        customItem: '全部'
     },
 
     /**
@@ -27,6 +34,97 @@ Page({
      */
     onShow: function () {
 
+    },
+
+    bindPickerChange(e) {
+        console.log('picker发送选择改变，携带值为', e);
+        this.setData({
+            index: e.detail.value
+        })
+    },
+
+    bindMultiPickerChange(e) {
+        console.log('picker发送选择改变，携带值为', e)
+        this.setData({
+            multiIndex: e.detail.value
+        })
+    },
+
+    bindMultiPickerColumnChange(e) {
+        console.log('修改的列为', e.detail.column, '，值为', e.detail.value, '整体值为', e);
+        var data = {
+            multiArray: this.data.multiArray,
+            multiIndex: this.data.multiIndex
+        }
+        data.multiIndex[e.detail.column] = e.detail.value;
+        switch (e.detail.column) {
+            case 0:
+                switch (data.multiIndex[0]) {
+                    case 0:
+                        data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
+                        data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+                        break;
+                    case 1:
+                        data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
+                        data.multiArray[2] = ['鲫鱼', '带鱼'];
+                        break;
+                }
+                data.multiIndex[1] = 0;
+                data.multiIndex[2] = 0;
+                break;
+            case 1:
+                switch (data.multiIndex[0]) {
+                    case 0:
+                        switch (data.multiIndex[1]) {
+                            case 0:
+                                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+                                break;
+                            case 1:
+                                data.multiArray[2] = ['蛔虫'];
+                                break;
+                            case 2:
+                                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
+                                break;
+                            case 3:
+                                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
+                                break;
+                            case 4:
+                                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
+                                break;
+                        }
+                        break;
+                    case 1:
+                        switch (data.multiIndex[1]) {
+                            case 0:
+                                data.multiArray[2] = ['鲫鱼', '带鱼'];
+                                break;
+                            case 1:
+                                data.multiArray[2] = ['青蛙', '娃娃鱼'];
+                                break;
+                            case 2:
+                                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
+                                break;
+                        }
+                        break;
+                }
+                data.multiIndex[2] = 0;
+                break;
+        }
+        this.setData(data);
+    },
+
+    bindTimeChange(e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            time: e.detail.value
+        })
+    },
+
+    bindRegionChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            region: e.detail.value
+        })
     },
 
     /**
